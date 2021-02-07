3de831de5e712d2daa7659e2fc319bf3491ca32f
William Ughetta <wughetta@princeton.edu>
2021-02-07 13:53:41 -0500

    Fix embed command. Make package name match dir

--- embed.go
+++ embed.go
@@ -1,15 +1,15 @@
-package static
+package explorer
 
 import _ "embed"
 
 //go:embed index.html
 var Index []byte
 
-// go:embed style.css
+//go:embed style.css
 var Style []byte
 
-// go:embed js/main.js
+//go:embed js/main.js
 var Main []byte
 
-// go:embed js/grid.js
+//go:embed js/grid.js
 var Grid []byte
